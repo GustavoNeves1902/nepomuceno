d87ce3fb1196beb5993632067338f7fe5198e514
Gustavo Neves <gustavohenrique19022@gmail.com>
2025-12-19 10:30:39 -0300

    movimento

--- scripts/index.js
+++ scripts/index.js
@@ -1,47 +1,68 @@
 document.querySelectorAll(".faq-item").forEach((item) => {
-    item.addEventListener("click", () => {
-      
-      // fecha todos os itens, menos o clicado
-      document.querySelectorAll(".faq-item").forEach((other) => {
-        if (other !== item) {
-          other.classList.remove("open");
-        }
-      });
-  
-      // abre/fecha o que você clicou
-      item.classList.toggle("open");
+  item.addEventListener("click", () => {
+    // fecha todos os itens, menos o clicado
+    document.querySelectorAll(".faq-item").forEach((other) => {
+      if (other !== item) {
+        other.classList.remove("open");
+      }
     });
+
+    // abre/fecha o que você clicou
+    item.classList.toggle("open");
   });
+});
 
-  const feedbacks = document.querySelectorAll('.feedback');
-  const prevBtn = document.getElementById('prevBtn');
-  const nextBtn = document.getElementById('nextBtn');
+const feedbacks = document.querySelectorAll(".feedback");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
 
-  let currentSlide = 0;
+let currentSlide = 0;
 
-  function showSlide(index) {
-    // Remove a classe active de todos
-    feedbacks.forEach(slide => slide.classList.remove('active'));
+function showSlide(index) {
+  // Remove a classe active de todos
+  feedbacks.forEach((slide) => slide.classList.remove("active"));
 
-    // Garante que o índice seja cíclico (loop infinito)
-    if (index >= feedbacks.length) {
-      currentSlide = 0;
-    } else if (index < 0) {
-      currentSlide = feedbacks.length - 1;
-    } else {
-      currentSlide = index;
-    }
-
-    // Adiciona a classe active no slide atual
-    feedbacks[currentSlide].classList.add('active');
+  // Garante que o índice seja cíclico (loop infinito)
+  if (index >= feedbacks.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = feedbacks.length - 1;
+  } else {
+    currentSlide = index;
   }
 
-  nextBtn.addEventListener('click', () => {
-    showSlide(currentSlide + 1);
-  });
+  // Adiciona a classe active no slide atual
+  feedbacks[currentSlide].classList.add("active");
+}
 
-  prevBtn.addEventListener('click', () => {
-    showSlide(currentSlide - 1);
-  });
+nextBtn.addEventListener("click", () => {
+  showSlide(currentSlide + 1);
+});
 
-  
+prevBtn.addEventListener("click", () => {
+  showSlide(currentSlide - 1);
+});
+
+ScrollReveal().reveal("#testimonial_teacher", {
+  origin: "right",
+  duration: 2000,
+  distance: "20%",
+});
+
+ScrollReveal().reveal("#teacher", {
+  origin: "right",
+  duration: 2000,
+  distance: "20%",
+});
+
+ScrollReveal().reveal("#texto", {
+  origin: "left",
+  duration: 2000,
+  distance: "20%",
+});
+
+ScrollReveal().reveal("#feedbacks", {
+  origin: "left",
+  duration: 2000,
+  distance: "20%",
+});
